5e503b637d3dfd088314cd0f1ae8d7e948a139b9
yurii-kurylenko <yuriy.kurilenko7@gmail.com>
2017-10-07 18:35:46 +0300

    Downgrade ruby to server

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,4 +6,4 @@ set :deploy_to, "/home/hound/linters-app"
 set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 set :keep_releases, 5
 
-set :rvm_ruby_version, '2.4.1'
+set :rvm_ruby_version, '2.3.4'
